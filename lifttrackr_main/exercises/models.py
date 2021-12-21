c8a77e7ab6259c6a0ea15f73cb05e5b41fb92a31
ian <iandavis6481@gmail.com>
2021-12-21 14:37:30 -0800

    migration fix

--- lifttrackr_main/exercises/models.py
+++ lifttrackr_main/exercises/models.py
@@ -8,7 +8,7 @@ from users.models import CustomUser
 class Exercise(models.Model):
     name = models.CharField(max_length=200)
     uuid = models.CharField(max_length=250)
-    description = models.CharField(max_length=600)
+    description = models.TextField()
     
 
     def __str__(self):
